9513e2d4708f3b343d0b30686c6fc3bf7d8a91dd
Franco Giachetta <fgiachetta@frba.utn.edu.ar>
2024-09-28 15:51:21 -0300

    predeploy

--- app/src/main.rs
+++ app/src/main.rs
@@ -21,7 +21,11 @@ async fn main() {
         .init();
     let port = env::var("PORT").expect("Missing port number");
     let port = port.parse::<u16>().expect("Invalid port given");
+
+    #[cfg(feature = "local")]
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+    #[cfg(not(feature = "local"))]
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
 
     let db = {
         #[cfg(not(feature = "local"))]
